705f43d60893b018bbbab55db8c81f41b81f3511
Pau PC <paucv27@gmail.com>
2026-04-26 22:27:45 +0100

    new blog post

--- data/blog-posts.ts
+++ data/blog-posts.ts
@@ -9,13 +9,27 @@ export type BlogPost = {
 export const blogPosts: BlogPost[] = [
   // TEMPLATE — copy this block to add a new post:
   {
+    id: "post-4",
+    title: "EL BAIFO",
+    date: "2026-04-26",     
+    tags: ["music"],
+    content: "EL BAIFO just released on friday, its an album by Canarian artist Quevedo, and its the only thing i've been listening to since then 𝄞𝄞\
+    honestly its so good, my favourite songs off it rn are 'EL BAIFO', 'HIJO DE VOLCAN', 'NI BORRACHO' and 'LA GRACIOSA'.\
+    its great hearing music made by your people for your people, its kinda like DTMF by Bad Bunny.\
+    it really makes you want to go back and be with your people, it makes you miss the things you thought you would always have,\
+     but its not so bad to feel like that when i know i'll be back soon even if its just for a little bit.\
+    point is, its great to reconnect, and dont fear the future, make it yourself :p\
+    un shoutout a todos los canarios del mundo por ser de un lugar tan bonito, y a cualquier persona que eche de menos estar en casa ✰"
+  },
+  {
     id: "post-3",
     title: "Production!",
     date: "2026-04-15",     
     tags: ["tech", "project", "update"],
     content: "just finished v1 of this webpage and deployed it! \
     im really happy with how its looking, and looking forward to implementing new features and sharing more content here if i dont forget... \
-    if you have any suggestions or feedback don't hesitate to reach out to me on any of my socials!"
+    if you have any suggestions or feedback don't hesitate to reach out to me on any of my socials!\
+    also i know there is no picture yet, i just cant find a good one and i dont want to use my linkedin one ✘"
   },
   {
     id: "post-2",
